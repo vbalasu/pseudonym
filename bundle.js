c4d43dd3eef2b5a497b5af5602bd728fdaa15d8e
vbalasu <vbalasu@gmail.com>
2021-09-18 17:21:46 -0700

    Write uuid4 to console

--- bundle.js
+++ bundle.js
@@ -7141,6 +7141,7 @@ exports.default = _default;
 var codenamize = require('@codenamize/codenamize');
 const { v4: uuidv4 } = require('uuid');
 let id = uuidv4();
+console.log(id);
 var result = codenamize(id);
 document.querySelector('#result').innerHTML = result;
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var codenamize = require('@codenamize/codenamize');
 const { v4: uuidv4 } = require('uuid');
 let id = uuidv4();
+console.log(id);
 var result = codenamize(id);
 document.querySelector('#result').innerHTML = result;
